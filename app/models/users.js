2ac50526e5ea36972e9e223015ab88252d393250
BBKolton <bbkolton@uw.edu>
2015-09-11 09:35:12 -0700

    Added User Model in Preperation for Metrics Tracking

---
+++ app/models/users.js
@@ -0,0 +1,15 @@
+module.exports = function (sequelize, DataTypes) {
+
+	var User = sequelize.define('User', {
+		NetId: 'VARCHAR(20)',
+		RegID: 'VARCHAR(40)',
+		FirstName: 'VARCHAR(30)',
+		LastName: 'VARCHAR(40)',
+		Permissions: 'TINYINT'
+	});
+
+	//PERMISSIONS//
+	//1 
+
+	return User;
+};
