bb6146de1b2710301420c2d248c9636064d80a34
Sanjay Sagar <sanjaysagarp@hotmail.com>
2016-08-25 14:47:55 -0700

    added dynamic data chart for funds

--- public/js/funds.js
+++ public/js/funds.js
@@ -3,10 +3,28 @@ $(document).ready(function(){
 	var proposals = JSON.parse($('#proposals').val());
 	var departments = JSON.parse($('#allDepartments').val());
 	var categories = JSON.parse($('#allCategories').val());
+	var fundedArr = ["Funded"];
+	var partiallyFunded = ["Partially Funded"];
+	var notFunded = ["Not Funded"];
+	var type = "pie";
 	var cat = "All";
 	var dept = "All";
 	var chart;
+	var totalProposals = 0;
+	var fundedProposals = 0;
+	var partialFunded = 0;
+	var funded = 0.0;
+	var requested = 0.0;
+	//initializes year axis
+	var yearAxis = ['x'];
+	for(year in proposals) {
+		var format = proposals[year][0].Year + '-01-01';
+		yearAxis.push(format);
+	}
+
 	refreshTable();
+
+	//change functions update table and chart
 	$("#department").change(function() {
 		dept = $(this).val();
 		refreshTable();
@@ -16,20 +34,117 @@ $(document).ready(function(){
 		cat = $(this).val();
 		refreshTable();
 	});
-	function refreshTable() {
-		var totalProposals = 0;
-		var fundedProposals = 0;
-		var partialFunded = 0;
-		var funded = 0.0;
-		var requested = 0.0;
+	$("#endYear").change(function() {
+		yearAxis = ['x'];
+		for(year in proposals) {
+			if(proposals[year][0].Year <= $("#endYear").val() && proposals[year][0].Year >= $("#startYear").val()) {
+				var format = proposals[year][0].Year + '-01-01';
+				yearAxis.push(format);
+			}
+		}
+		refreshTable();
+	});
+	$("#startYear").change(function() {
+		yearAxis = ['x'];
+		for(year in proposals) {
+			if(proposals[year][0].Year <= $("#endYear").val() && proposals[year][0].Year >= $("#startYear").val()) {
+				var format = proposals[year][0].Year + '-01-01';
+				yearAxis.push(format);
+			}
+		}
+		refreshTable();
+	});
 
-		//unload current chart if exists
+	$("#chartType").change(function() {
+		type = $(this).val();
+		if(type == "pie") {
+			chart = c3.generate({
+				bindto: '#chart',
+				data: {
+					columns: [
+						["Not Funded", (totalProposals - (fundedProposals+partialFunded))],
+						["Partially Funded", partialFunded],
+						["Funded", fundedProposals],
+					],
+					type : "pie",
+				},
+				size: {
+					height: 600,
+					width: 800
+				},
+				padding: {
+					top: 50
+				},
+				tooltip: {
+					format: {
+						value: function (value, ratio, id) {
+							return value + " Proposals";
+						}
+					}
+				},
+				pie: {
+					expand: true
+				}
+			});
+		} else {
+			chart = c3.generate({
+					bindto: '#chart',
+					data: {
+						x: 'x',
+						columns: [
+							yearAxis,
+							notFunded,
+							partiallyFunded,
+							fundedArr,
+						],
+						type : type,
+					},
+					size: {
+						height: 600,
+						width: 800
+					},
+					padding: {
+						top: 50
+					},
+					tooltip: {
+						format: {
+							value: function (value, ratio, id) {
+								return "$" + value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+							}
+						}
+					},
+					axis : {
+						x : {
+							type : 'timeseries',
+							tick: {
+								format: function (x) { return x.getFullYear(); }
+							}
+						},
+						y : {
+							tick: {
+								format: d3.format("$,")
+							}
+						}
+					}
+				});
+		}
+	});
+
+	
+	function refreshTable() {
+		totalProposals = 0;
+		fundedProposals = 0;
+		partialFunded = 0;
+		funded = 0.0;
+		requested = 0.0;
+
 		if(chart == undefined) {
+			//initialize pie
 			chart = c3.generate({
 				bindto: '#chart',
 				data: {
 					columns: [],
-					type : 'pie',
+					type : "pie",
 				},
 				size: {
 					height: 600,
@@ -51,31 +166,91 @@ $(document).ready(function(){
 			});
 		}
 		
+		var startYear = $('#startYear').val();
+		var endYear = $('#endYear').val();
+		fundedArr = ["Funded"];
+		partiallyFunded = ["Partially Funded"];
+		notFunded = ["Not Funded"];
+
 		for(year in proposals) {
-			for(num in proposals[year]) {
-				if((cat == "All" || ( categories[proposals[year][num].Category] != null && categories[proposals[year][num].Category].name == categories[cat].name)) && (dept == "All" || proposals[year][num].Department == departments[dept])) {
-					if(proposals[year][num].Decision != "Not Funded") {
-						if(proposals[year][num].Decision == "Funded") {
-							fundedProposals++;
+			var currFund = 0, currPartial = 0, currNotFund = 0;
+			if(proposals[year][0].Year >= startYear && proposals[year][0].Year <= endYear) {
+				for(num in proposals[year]) {
+					if((cat == "All" || ( categories[proposals[year][num].Category] != null && categories[proposals[year][num].Category].name == categories[cat].name)) && (dept == "All" || proposals[year][num].Department == departments[dept])) {
+						if(proposals[year][num].Decision != "Not Funded") {
+							if(proposals[year][num].Decision == "Funded") {
+								fundedProposals++;
+								currFund += proposals[year][num].Award;
+							} else {
+								partialFunded++;
+								currPartial += proposals[year][num].Award;
+							}
+							funded += proposals[year][num].Award;
 						} else {
-							partialFunded++;
+							currNotFund += proposals[year][num].Requested;
 						}
-						funded += proposals[year][num].Award;
+						requested += proposals[year][num].Requested;
+						totalProposals++;
 					}
-					requested += proposals[year][num].Requested;
-					totalProposals++;
 				}
+				fundedArr.push(currFund);
+				partiallyFunded.push(currPartial);
+				notFunded.push(currNotFund);
 			}
 			//gives that loading effect
 			//if bar, pie, line
-			chart.load({
-				unload: ["Not Funded", "Partially Funded", "Funded"],
-				columns: [
-					["Not Funded", (totalProposals - (fundedProposals+partialFunded))],
-					["Partially Funded", partialFunded],
-					["Funded", fundedProposals],
-				]
-			});
+			if(type == 'pie') {
+				chart.load({
+					unload: ["Not Funded", "Partially Funded", "Funded"],
+					columns: [
+						["Not Funded", (totalProposals - (fundedProposals+partialFunded))],
+						["Partially Funded", partialFunded],
+						["Funded", fundedProposals],
+					]
+				});
+			} 
+		}
+		if(type != 'pie') {
+			chart = c3.generate({
+					bindto: '#chart',
+					data: {
+						x: 'x',
+						columns: [
+							yearAxis,
+							notFunded,
+							partiallyFunded,
+							fundedArr,
+						],
+						type : type,
+					},
+					size: {
+						height: 600,
+						width: 800
+					},
+					padding: {
+						top: 50
+					},
+					tooltip: {
+						format: {
+							value: function (value, ratio, id) {
+								return "$" + value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+							}
+						}
+					},
+					axis : {
+						x : {
+							type : 'timeseries',
+							tick: {
+								format: function (x) { return x.getFullYear(); }
+							}
+						},
+						y : {
+							tick: {
+								format: d3.format("$,")
+							}
+						}
+					}
+				});
 		}
 
 		$('#requested').html("$" + requested.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
