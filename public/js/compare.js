454445eef534e92716236a73c2eb9afcb6e3917e
Sanjay Sagar <sanjaysagarp@hotmail.com>
2016-08-31 14:00:50 -0700

    departmental funding comparison chart added

--- public/js/compare.js
+++ public/js/compare.js
@@ -3,10 +3,7 @@ $(document).ready(function(){
 	var proposals = JSON.parse($('#proposals').val());
 	var departments = JSON.parse($('#allDepartments').val());
 	var categories = JSON.parse($('#allCategories').val());
-	var fundedArr = ["Funded"];
-	var partiallyFunded = ["Partially Funded"];
-	var notFunded = ["Not Funded"];
-	var type = "pie";
+	var type = "line";
 	var cat = "All";
 	var dept = "All";
 	var chart;
@@ -15,6 +12,8 @@ $(document).ready(function(){
 	var partialFunded = 0;
 	var funded = 0.0;
 	var requested = 0.0;
+	var departmentArr = [];
+
 	//initializes year axis
 	var yearAxis = ['x'];
 	for(year in proposals) {
@@ -24,9 +23,9 @@ $(document).ready(function(){
 
 	refreshTable();
 
-	//change functions update table and chart
-	$("#department").change(function() {
-		dept = $(this).val();
+	$('#addDepartment').on('click', function() {
+		dept = $('#department').val();
+		refreshTable();
 	});
 
 	$("#category").change(function() {
@@ -35,69 +34,34 @@ $(document).ready(function(){
 	});
 
 	$("#endYear").change(function() {
-		yearAxis = ['x'];
-		for(year in proposals) {
-			if(proposals[year][0].Year <= $("#endYear").val() && proposals[year][0].Year >= $("#startYear").val()) {
-				var format = proposals[year][0].Year + '-01-01';
-				yearAxis.push(format);
-			}
-		}
-		refreshTable();
+		adjustAxis();
 	});
 
 	$("#startYear").change(function() {
-		yearAxis = ['x'];
-		for(year in proposals) {
-			if(proposals[year][0].Year <= $("#endYear").val() && proposals[year][0].Year >= $("#startYear").val()) {
-				var format = proposals[year][0].Year + '-01-01';
-				yearAxis.push(format);
-			}
-		}
-		refreshTable();
+		adjustAxis();
+	});
+
+	$("#clearAll").on('click', function() {
+		departmentArr = [];
+		initializeChart();
 	});
 
 	$("#chartType").change(function() {
 		type = $(this).val();
-		chart = c3.generate({
-				bindto: '#chart',
-				data: {
-					x: 'x',
-					columns: [
-						yearAxis,
-						notFunded,
-						partiallyFunded,
-						fundedArr,
-					],
-					type : type,
-				},
-				size: {
-					height: 600,
-					width: 800
-				},
-				padding: {
-					top: 50
-				},
-				tooltip: {
-					format: {
-						value: function (value, ratio, id) {
-							return "$" + value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-						}
-					}
-				},
-				axis : {
-					x : {
-						type : 'timeseries',
-						tick: {
-							format: function (x) { return x.getFullYear(); }
-						}
-					},
-					y : {
-						tick: {
-							format: d3.format("$,")
-						}
-					}
-				}
+		initializeChart();
+
+		for(var i = 0; i < departmentArr.length; i++) {
+			chart.load({
+				unload: [
+					yearAxis,
+					departmentArr[i]
+				],
+				columns:[
+					yearAxis,
+					departmentArr[i]
+				]
 			});
+		}
 	});
 
 	
@@ -109,8 +73,61 @@ $(document).ready(function(){
 		requested = 0.0;
 
 		if(chart == undefined) {
-			//initialize bar graph to all funded?
-			chart = c3.generate({
+			initializeChart();
+		}
+		
+		var startYear = $('#startYear').val();
+		var endYear = $('#endYear').val();
+		if(dept == "All") {
+			currArray = [dept];
+		} else {
+			currArray = [departments[dept]];
+		}
+		
+
+		// TODO - only get dollar amounts for comparisons
+		for(year in proposals) {
+			if(proposals[year][0].Year >= startYear && proposals[year][0].Year <= endYear) {
+				for(num in proposals[year]) {
+					if((cat == "All" || ( categories[proposals[year][num].Category] != null && categories[proposals[year][num].Category].name == categories[cat].name)) && (dept == "All" || proposals[year][num].Department == departments[dept])) {
+						if(proposals[year][num].Decision != "Not Funded") {
+							funded += proposals[year][num].Award;
+						}
+						totalProposals++;
+					}
+				}
+				currArray.push(funded);
+				funded = 0;
+			}
+		}
+		departmentArr.push(currArray);
+
+
+		for(var i = 0; i < departmentArr.length; i++) {
+			chart.load({
+				unload: [
+					yearAxis,
+					departmentArr[i]
+				],
+				columns:[
+					yearAxis,
+					departmentArr[i]
+				]
+			});
+		}
+	}
+
+	function contains(depts, curr) {
+		for(var i = 0; i < depts.length; i++) {
+			if(depts[i][0] == curr[0] && depts[i].length == curr.length) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function initializeChart() {
+		chart = c3.generate({
 				bindto: '#chart',
 				data: {
 					x: 'x',
@@ -147,91 +164,57 @@ $(document).ready(function(){
 					}
 				}
 			});
-		}
-		
-		var startYear = $('#startYear').val();
-		var endYear = $('#endYear').val();
-		currArray = [dept];
+	}
 
-		// TODO - only get dollar amounts for comparisons
+	function adjustAxis() {
+		yearAxis = ['x'];
 		for(year in proposals) {
-			var currFund = 0, currPartial = 0, currNotFund = 0;
-			if(proposals[year][0].Year >= startYear && proposals[year][0].Year <= endYear) {
-				for(num in proposals[year]) {
-					if((cat == "All" || ( categories[proposals[year][num].Category] != null && categories[proposals[year][num].Category].name == categories[cat].name)) && (dept == "All" || proposals[year][num].Department == departments[dept])) {
-						if(proposals[year][num].Decision != "Not Funded") {
-							funded += proposals[year][num].Award;
-						}
-						totalProposals++;
-					}
-				}
-				currArray.push(funded);
+			if(proposals[year][0].Year <= $("#endYear").val() && proposals[year][0].Year >= $("#startYear").val()) {
+				var format = proposals[year][0].Year + '-01-01';
+				yearAxis.push(format);
 			}
 		}
 
-		chart.load({
-			columns:[
-				currArray
-			]
-		});
+		var startYear = $('#startYear').val();
+		var endYear = $('#endYear').val();
+		funded = 0;
 
-		//need to keep track with whatever is loaded -- Display which departments are in the graph with an x icon to get rid of it
-
-		// chart = c3.generate({
-		// 		bindto: '#chart',
-		// 		data: {
-		// 			x: 'x',
-		// 			columns: [
-		// 				yearAxis,
-		// 				currArray,
-		// 			],
-		// 			type : type,
-		// 		},
-		// 		size: {
-		// 			height: 600,
-		// 			width: 800
-		// 		},
-		// 		padding: {
-		// 			top: 50
-		// 		},
-		// 		tooltip: {
-		// 			format: {
-		// 				value: function (value, ratio, id) {
-		// 					return "$" + value.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-		// 				}
-		// 			}
-		// 		},
-		// 		axis : {
-		// 			x : {
-		// 				type : 'timeseries',
-		// 				tick: {
-		// 					format: function (x) { return x.getFullYear(); }
-		// 				}
-		// 			},
-		// 			y : {
-		// 				tick: {
-		// 					format: d3.format("$,")
-		// 				}
-		// 			}
-		// 		}
-		// 	});
-
-		$('#requested').html("$" + requested.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-		$('#funded').html("$" + funded.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","));
-		if(totalProposals != 0) {
-			$('#percent').html((100*((fundedProposals + partialFunded)/totalProposals)).toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + "%");
-		} else {
-			$('#percent').html("0%");
+		departmentRefresh = [];
+		for(index in departmentArr) {
+			currArray = [departmentArr[index][0]];
+			for(year in proposals) {
+				if(proposals[year][0].Year >= startYear && proposals[year][0].Year <= endYear) {
+					for(num in proposals[year]) {
+						if((cat == "All" || ( categories[proposals[year][num].Category] != null && categories[proposals[year][num].Category].name == categories[cat].name)) && (departmentArr[index][0] == "All" || proposals[year][num].Department == departmentArr[index][0])) {
+							if(proposals[year][num].Decision != "Not Funded") {
+								funded += proposals[year][num].Award;
+							}
+						}
+					}
+					currArray.push(funded);
+					funded = 0;
+				}
+			}
+			departmentRefresh.push(currArray);
+			chart.load({
+				unload:[
+					yearAxis,
+					departmentArr[index]
+				]
+			});
 		}
-		var catName = cat;
-		var deptName = dept + " Departments";
-		if(cat != "All") {
-			catName = categories[cat].name;
+		departmentArr = departmentRefresh;
+		for(var i = 0; i < departmentRefresh.length; i++) {
+			chart.load({
+				unload: [
+					yearAxis,
+					departmentRefresh[i]
+				],
+				columns:[
+					yearAxis,
+					departmentRefresh[i]
+				]
+			});
 		}
-		if(dept != "All") {
-			deptName = departments[dept];
-		}
-		
-		$('#title').html(deptName + ", Category: " + catName);
 	}
 });
